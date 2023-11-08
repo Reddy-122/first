ef703c4dbaa6a80a2c6d4b489239edca81aee43d
Reddy-12 <vishnuvardanreddy1818@gmail.com>
2023-11-08 17:10:39 +0530

    first commit

--- app3/views.py
+++ app3/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def srinu(request):
+    return HttpResponse('<h1><marquee>hi ra</marquee></h1>')
